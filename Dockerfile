2bd8566702d5cffe5ace7178267ade4296b274f9
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-01-22 20:20:46 -0300

    Initial structure of spring dockerized application

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM openjdk:17-alpine3.14
+
+RUN apk add --no-cache bash
+
+WORKDIR /app
+
+COPY .mvn/ .mvn
+COPY mvnw pom.xml ./
+RUN ./mvnw dependency:go-offline
+
+COPY src ./src
+
+CMD ["./mvnw", "spring-boot:run"]
---
+++ src/main/java/br/com/ajvideira/algafood/AlgafoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.ajvideira.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApplication.class, args);
+	}
+
+}
