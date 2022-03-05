e74b671279bbd2617202550e984e88eeed80b5b2
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-05 19:18:13 -0300

    Using @Autowired in different locations within component

--- src/main/java/br/com/ajvideira/service/AtivacaoClienteService.java
+++ src/main/java/br/com/ajvideira/service/AtivacaoClienteService.java
@@ -1,23 +1,37 @@
 package br.com.ajvideira.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import br.com.ajvideira.model.Cliente;
 import br.com.ajvideira.notificacao.Notificador;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-	Notificador notificador;
+	//@Autowired
+	private Notificador notificador;
 	
+	@Autowired
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
 		
 		System.out.println("AtivacaoClienteService: " + notificador);
 	}
 	
+	public AtivacaoClienteService(String teste) {
+		
+	}
+	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+	
+	//@Autowired
+	public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
 	}
 	
 }
