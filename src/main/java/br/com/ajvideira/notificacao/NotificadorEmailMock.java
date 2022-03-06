880b059128235eeb097f58ce18f9a8d6e3355fea
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-05 22:55:20 -0300

    Alter app configs based on profile

--- src/main/java/br/com/ajvideira/notificacao/NotificadorEmailMock.java
+++ src/main/java/br/com/ajvideira/notificacao/NotificadorEmailMock.java
@@ -1,5 +1,6 @@
 package br.com.ajvideira.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +11,15 @@ import br.com.ajvideira.model.Cliente;
 @Component
 public class NotificadorEmailMock implements Notificador {
 
+	@Autowired
+	private NotificadorProperties notificadorProperties;
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 
+		System.out.println("Host: " + notificadorProperties.getHostServidor());
+		System.out.println("Porta: " + notificadorProperties.getPortaServidor());
+		
 		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
