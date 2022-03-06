e8d01dd7cb973d042e6e3a70c69b2bde49f35c97
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-05 22:37:30 -0300

    Using custom properties with @Value

--- src/main/java/br/com/ajvideira/notificacao/NotificadorEmail.java
+++ src/main/java/br/com/ajvideira/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package br.com.ajvideira.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +11,18 @@ import br.com.ajvideira.model.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
